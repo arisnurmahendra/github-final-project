0bb490710522683209cbd5e850cccc2b31d8c2d9
Dragana Neshevska <dragana.neshevska@gmail.com>
2025-02-06 16:24:27 -0500

    added a new line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,3 +11,10 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
  # Here is a new update by Dragana Neshevska
+if shipping_cost > 100:
+    discount = shipping_cost * 0.10  # 10% discount
+    shipping_cost -= discount
+    print(f"A 10% discount has been applied! New Shipping Cost: {shipping_cost:.2f} USD")
+
+    
+
