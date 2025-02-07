94e70c7b1a89d0fb8f3c0cb414a44085b7b1c72b
Jatin <ronak9660@gmail.com>
2025-02-07 04:55:11 -0500

    Revert "new changes"
    This reverts commit b20a49d20928439f5a2596cfe421916ca0f33799.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,7 +1,6 @@
 # Shipping Cost Calculator
 
 # Here is a new update by <jatin0902>
-# Here is another update by <jatin0902>
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
