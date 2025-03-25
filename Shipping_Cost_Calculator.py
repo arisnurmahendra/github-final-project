883b2207d10debf09667317159143c87e6b0e722
German <germancardenas@gmail.com>
2025-03-25 12:03:39 -0400

    aanother commit

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,11 +1,12 @@
 # Here is a new update by germancardenas@gmail.com
+# Here is a another update by germancardenas@gmail.com
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
 
-## Calculate shipping cost
+## Calculate shipping costco 
 shipping_cost = weight * rate
 
 ## Display the result
