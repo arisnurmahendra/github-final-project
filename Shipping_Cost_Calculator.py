f7bd112c7d1c64817452ce7a0ed6ed39d467ccad
lilyjykim <lilliank813@nate.com>
2025-04-16 07:58:12 -0400

    Revert "second commit test"
    This reverts commit 09fda5b40a696d9ba8322d7891026a75841ff978.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,5 +11,3 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
 # Here is a new update by lilyjykim
-#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
-second update to files
