79ebdbe3e93e72767af214b722de50330fd8cdae
MNasarFareed <nasarfareed04@gmail.com>
2025-04-25 01:14:29 +0500

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit f955d1b8da8f0bad92cc784394fd50837b638061.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,7 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by MNasarFareed
-
- # Here is another update by MNasarFareed
- 
+ # Here is a new update by <your GitHub username>
