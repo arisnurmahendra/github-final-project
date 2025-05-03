6a27bb19ef05b15a33ac293f86317494432ae87e
Vedran Marin <vedran.marin@gmail.com>
2025-05-03 17:49:42 +0200

    added new functionality in .py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,3 +10,6 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
+# it is a pleasure working with you
+
+print("programming is a piece of hell ;) ")
