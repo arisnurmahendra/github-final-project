c95f611804aba81835a1f535a193de756fb72355
Jose <jose.osoiroyforeta@limagrain.com>
2025-05-22 11:07:29 -0400

    Revert "2nd test"
    This reverts commit 644c3c72bab43b541c9291035a43e06280af0d7b.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,5 +1,4 @@
-# Here is another update by <your GitHub username>
-# Here is a new update by <your GitHub username>
+ # Here is a new update by <your GitHub username>
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
