ff63a43c9dfa68fababd15ec0becd1c5ee139453
sangel07 <sova970716@gmail.com>
2025-05-27 16:59:07 -0400

    added a new line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,3 +10,9 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
+# Aquí hay una nueva actualización por Sangel07
+git add Shipping_Cost_Calculator.py
+git commit -m "Agregada línea con nombre de usuario GitHub"
+git push origin shipping_calculator_fixes
+^X
+
