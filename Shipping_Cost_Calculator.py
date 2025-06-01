268351468efab9aa65a707acd2acf32c3ce31801
Rin Koenig <karin.m.koenig@gmail.com>
2025-06-01 17:18:33 -0400

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,4 +10,5 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by rinkoenig5
+ # Here is a new update by rinkoenig58
+ # Here is another update by rinkoenig58
