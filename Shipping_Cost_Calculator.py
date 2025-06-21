c2729f81e1a52762a4d947ce32fe1c2eda9f7732
Abdullah Salah <asr12211@outlook.com>
2025-06-21 15:35:35 +0300

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,4 +10,6 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
-     # Here is a new update by asr1221
+# Here is a new update by asr1221
+
+# Here is another update by asr1221
