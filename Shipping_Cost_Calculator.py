b65054eea2ede9832f06b2502e947a7cd849669b
rmalekar <rmalekar1992@gmail.com>
2025-07-09 13:40:31 +1000

    Added validation part for input

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,7 +1,13 @@
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
-weight = float(input("Enter the package weight in kilograms: "))
+weight = None
+while True:
+    try:
+        weight = float(input("Enter the package weight in kilograms: "))
+        break
+    except ValueError:
+        print("Invalid Input. Please enter a valid weight: ")
 rate = float(input("Enter the shipping rate per kilogram: "))
 
 ## Calculate shipping cost
