090e763db882251267d4282cd374504ade7296ff
Rault2242z <rault2242z@hotmail.com>
2025-07-18 19:01:54 -0400

    agregada linea adicional a Shipping_Cot_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,5 +1,6 @@
 # Shipping Cost Calculator
  # Aquí hay una nueva actualización por <Rault2242z>
+ # Here is another update by <Rault2242z>
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
