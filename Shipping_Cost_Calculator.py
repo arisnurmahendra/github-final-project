7633b4bf80bdbdf5f8b937efdf68ef2bbbbcbda4
Suleeyman <ozturkyavuz2412@gmail.com>
2025-07-20 17:51:32 +0200

    Revert "Hello World Comment"
    This reverts commit 5fd16f4994e41f02b986676d33e0d1aa41df28cd.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,6 +1,5 @@
 # Shipping Cost Calculator
 # Here is a new update by <your GitHub username>
-# Hello world !
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
