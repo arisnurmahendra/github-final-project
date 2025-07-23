5abaae64d3b26287e1ab66e5b62e941a25464dd5
HarryRamirez <kennyharry47@gmail.com>
2025-07-23 17:52:57 -0500

    FIX

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,8 +1,31 @@
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
-weight = float(input("Enter the package weight in kilograms: "))
-rate = float(input("Enter the shipping rate per kilogram: "))
+while True:
+    
+    try:
+        weight = float(input("Enter the package weight in kilograms: "))
+        if weight > 0:
+            break
+        else:
+            print('The value must be a number')
+    except ValueError:
+        print("Invalid input: it must be a number")
+            
+while True:
+    
+    try:
+        rate = float(input("Enter the shipping rate per kilogram: "))
+        if rate > 0:
+            break
+        else:
+            print('The value must be a number')
+    except ValueError:
+        print("Invalid input: it must be a number")
+
+        
+
+    
 
 ## Calculate shipping cost
 shipping_cost = weight * rate
