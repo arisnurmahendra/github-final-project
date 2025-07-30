32ad774d9cedee4ced24505c94ac95d8793cd84a
melvin-phyllis <melvinphyllisakou@gmail.com>
2025-07-30 09:04:43 +0000

    added an extra line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,6 +1,7 @@
 # Shipping Cost Calculator
 # Voici une nouvelle mise à jour par <your GitHub melvin-phyllis>
 ## Input package weight and shipping rate
+# Here is another update by <your GitHub melvin-phyllis>
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
 
