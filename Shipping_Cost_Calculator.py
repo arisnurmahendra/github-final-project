5c4dcf267effbc790fbf1aed57268773041f6663
Srirambala2k <bsriram2k@gmail.com>
2025-08-06 02:37:32 -0400

    Added another update comment by Srirambala2k

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,3 +10,5 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 print("my user name is Srirambala2k")
+
+# Here is another update by Srirambala2k
