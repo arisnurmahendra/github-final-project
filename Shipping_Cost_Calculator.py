61d2033c76a70a7237f8d5d2421a565f2c381851
BLW-Code <blwilson2003@gmail.com>
2025-08-11 13:18:16 -0400

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit 89965e7639d5c2b1b62caaab2b01c14abfd54048.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,6 +1,5 @@
 # Shipping Cost Calculator
 #Here is a new update by BLW-Code
-#Here is another update by BLW-Code
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
