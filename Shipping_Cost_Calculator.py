89e110733e67e922b0f087ca4a440370b49e3973
sk <steff15@gmx.de>
2025-08-20 11:18:41 +0200

    added a new line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,3 +1,5 @@
+ # Here is a new update by <your GitHub steff155>
+
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
@@ -9,4 +11,5 @@ shipping_cost = weight * rate
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
+
 
