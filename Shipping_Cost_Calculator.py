898b35e0812c74e4a7804dddc18d968720255fdd
nanneisan <khinmaykyaw317165@gmail.com>
2025-08-20 03:32:13 -0400

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit ea0772c4445e7851b6d1f33f6f08d01d77f13094.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,6 +1,5 @@
 # Shipping Cost Calculator
 # Here is a new update by nanneisan
-# Here is another update by nanneisan
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
