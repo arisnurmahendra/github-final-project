16b60ae16cea83b3c7aa9c3cb151a40e8cadf669
DammySmartSolutions <osademiluyi@mun.ca>
2025-08-24 23:41:05 -0230

    updated Shipping cost

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -7,6 +7,8 @@ rate = float(input("Enter the shipping rate per kilogram: "))
 ## Calculate shipping cost
 shipping_cost = weight * rate
 
+shipping_cost_CAD = shipping_cost * 1.37
+
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
-
+print(f"Shipping Cost in CAD: {shipping_cost_CAD} CAD")
