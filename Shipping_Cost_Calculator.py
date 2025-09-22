9b8166d748391bf9ae7a636d65752ebbcd8f91ba
sharmila-udaiyar <sharmilaudaiyar@gmail.com>
2025-09-22 21:56:20 +0530

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit 6296bbc99bb4e00449ebce31e3d206df1064421b.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,5 +1,4 @@
-
-# Here is a new update by Sharmila Udaiyar
+ # Here is a new update by Sharmila Udaiyar
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
@@ -12,4 +11,3 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
-# Here is another update by Sharmila Udaiyar
