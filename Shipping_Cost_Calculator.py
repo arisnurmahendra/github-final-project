51d6177aaf9616fb6adbc8bd32520ff5d1ab288a
Omar-adel11 <omaradel1258@gmail.com>
2025-09-27 02:52:54 +0300

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit 4040684b856e1a0be6849c7b66920b3787ceedc9.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,4 +11,4 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
  # Here is a new update by Omar-adel11
-p# Here is another update by Omar-adel11
+print("Thank you for using the Shipping Cost Calculator!")
