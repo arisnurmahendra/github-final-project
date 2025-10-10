5d95928af7ce8d98459b6df53e428222400e39ab
Kenley <kbarrett@stratadecision.com>
2025-10-10 14:44:40 -0400

    Another commit

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,6 +1,6 @@
 # Shipping Cost Calculator
 # Here is a new update by kmb-data
-
+# Here is another update by kmb-data
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
