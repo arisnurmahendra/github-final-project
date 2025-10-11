6cb956fa513966fa92b7d092a3958688195018f0
khanhtdk <khanhtd@gmail.com>
2025-10-11 17:04:16 +0700

    Revert "updating Shipping_Cost_Calculator.py"
    This reverts commit 2dd9633509935969a2dac3c96e02cd88be2ae523.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,3 +1,5 @@
+ # Here is a new update by khanhtdk
+
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
@@ -10,6 +12,3 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
-# Here is a new update by khanhtdk
-
-# Here is another update by khanhtdk
