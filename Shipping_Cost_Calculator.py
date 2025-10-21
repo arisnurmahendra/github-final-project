23fbddce703aed632fabdb3f91f9941e67e3a852
EliasBlake55 <muhdilyas360@gmail.com>
2025-10-21 10:48:44 -0400

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,4 +1,5 @@
- # Here is a new update by Eliasblake55
+# Here is a new update by Eliasblake55
+# Here is another update by <your GitHub username>
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
