50d5bc966f0617aff1a619ff404c9f53ed68e7b4
KuruGit <david.breier@outlook.de>
2025-10-26 12:53:13 -0400

    Revert "added another line"
    This reverts commit 1da4df78bf6276780f81d48f95ad51a3de83e914.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -2,8 +2,6 @@
 
  # Here is a new update by KuruGit
 
-# Here is another update by KuruGit
-
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
