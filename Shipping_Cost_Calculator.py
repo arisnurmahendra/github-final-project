c084352ce6a9ba39bd771572f7ef362d3e64d0f9
maplerocks <samuel.ting942gmail.com>
2025-10-27 08:29:36 -0400

    Revert "A new comment line added in Shipping_Cost_Calculator.py"
    This reverts commit 76dc977705a3bb3d2e2e438a8becd7b6ccdff7c3.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,6 +1,5 @@
 # Shipping Cost Calculator
 # Here is a new update by maplerocks
-# Here is another new update by maplerocks
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
