dfb946dcd0ca692f75ac0aeb2f0bc7a67dfd5f07
Jean-Luc-of-God <kwizerajeanluc30@gmail.com>
2025-10-30 14:23:37 -0400

    Revert "I added a new update"
    This reverts commit a788b2cbf3b328ff283ee2d6f007aa889553526d.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,5 +11,4 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
  # Here is a new update by Jean-Luc-of-God # Here is a new update by Jean-Luc-of-God
- # Here is another update by Jean-Luc-of-God
 
