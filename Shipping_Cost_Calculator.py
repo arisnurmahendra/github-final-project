e89fa9f56109e035f438e6a0726f15d885dedb30
Sabina Abdullina <141624969+SabinaAbd777@users.noreply.github.com>
2025-11-13 11:21:56 +0100

    Update Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,3 +10,9 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
+# Here is a new update by SabinaAbd777
+git config --global user.email "sabina.adbd@email.cz"
+git config --global user.name "SabinaAbd777"
+git add .
+git commit -m "added a new line to Shipping_Cost_Calculator.py"
+git status
