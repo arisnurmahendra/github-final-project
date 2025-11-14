7b395f05d7cd8a80f29e07ac6723e8b280dfc97b
giovannisbelizario-lab <giovanni.s.belizario@gmail.com>
2025-11-14 11:22:31 -0500

    adicionada linha adicional ao Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,3 +11,4 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
 # Aqui está uma nova atualização por giovannisbelizario-lab
+# Here is another update by giovannisbelizario
