6b17fbdecc9fa48cd4878ae912fa5c16cf7a1d57
jahid230 <jahid.crypto@gmail.com>
2025-11-24 05:40:37 -0500

    Revert "new update from jahid230"
    This reverts commit 9ea9d21c4ed29d5fbfcfaa7f5f333ad480a3280a.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,5 +1,5 @@
 # Shipping Cost Calculator
-#Here is the update for jahid230g
+#Here is the update for jahid230
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
