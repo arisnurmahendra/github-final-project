ae13a1d2d470fc2a6b7a46ae632d97731ac047ba
nicosdiogo <nicosdiogo@gmail.com>
2026-01-08 20:00:52 +0100

    added a new line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,4 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Aqui está uma nova atualização por <seu nome de usuário do GitHub>
+ # Aqui está uma nova atualização por nicosdiogo
