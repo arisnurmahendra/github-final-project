5c17698aa84f2eac43f13db4b209d959936a2368
jblandin-art <jblandin@charlotte.edu>
2026-01-20 13:34:48 -0500

    Revert "ieijfidsanf"
    This reverts commit 673ffa6b159a3d110057832af28eacbe14c38c9f.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -2,8 +2,6 @@
 
 # Here is a new update by jblandin-art
 
-# Here is a new update by jblandin-art
-
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
