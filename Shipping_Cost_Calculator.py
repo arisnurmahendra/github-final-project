9b8eb0752948eff557022e918d9cff81a0b3a6c3
ujju16 <jul.darrouza@gmail.com>
2026-01-21 08:00:56 -0500

    Revert "added a new line in script"
    This reverts commit d40bac230bafbf25d03a9574fff94be4c2a5ccd8.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,7 +1,6 @@
 # Shipping Cost Calculator
 
 # Here is abnew update by ujju16
-# Here is abnew update by ujju16
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
