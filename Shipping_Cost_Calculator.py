b74ed5d1936fbf95b5c4a5f8ba096ead70f1d737
Lance <lnickel@mailbox.sc.edu>
2026-01-28 08:33:07 -0500

    Revert "added another"
    This reverts commit 653b718c2f09252bc2887cc6c22911b428960daf.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,6 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- 
  # Here is a new update by <lance1572>
-# Here is another update by <lance1572>
