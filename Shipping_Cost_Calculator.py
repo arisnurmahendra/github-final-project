385d6fd91fffdc703b717e423fbe928aa5af509c
charfeddine1997 <azizcharfeddine1997@gmail.com>
2026-01-27 23:30:10 -0500

    add new update to Shipping_Cost_Calculator file

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,4 +1,5 @@
- # Here is a new update by Charfeddine1997
+# Here is a new update by Charfeddine1997
+# Here is another update by Charfeddine1997
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
