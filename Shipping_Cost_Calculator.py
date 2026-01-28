ec4936f86e11e471fc06917e9e26cc73aec3e4d8
maniakalen <maniakalen@gmail.com>
2026-01-28 08:50:18 -0500

    Revert "new code"
    This reverts commit 8190000b95620ff2c9a495a3942cc48ae934001c.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,9 +11,3 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
  # Here is a new update by maniakalen
-  Here is another update by <your GitHub username>
-
-Copied!
-
-Wrap Toggled!
-
