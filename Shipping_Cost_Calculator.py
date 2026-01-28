653b718c2f09252bc2887cc6c22911b428960daf
Lance <lnickel@mailbox.sc.edu>
2026-01-28 08:32:19 -0500

    added another

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,4 +10,6 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
+ 
  # Here is a new update by <lance1572>
+# Here is another update by <lance1572>
