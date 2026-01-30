389d4e315f30e74f93e0b85823431bb14ce1d1cb
karthika vennilavan <karunya.vennilavan@gmail.com>
2026-01-30 08:19:45 -0500

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit 2ced9246fe3e72108aa53c3735ed6ba21f7eb289.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -13,12 +13,6 @@ print(f"Shipping Cost: {shipping_cost} USD")
  # Here is a new update by <karthikavennilavan>
  print(f"Shipping Created Successfully")
 
- # Here is another update by <your GitHub username>
-
-git add .
-
-
-
 
 
 
