2ff922369a43c1190f622296a7afbedeb22536e9
Prathap goud <65637706+pandugoud@users.noreply.github.com>
2026-02-01 17:49:57 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,12 @@
+echo "Principal amount enter cheyyandi:"
+read principal
 
+echo "Rate of Interest enter cheyyandi:"
+read rate
+
+echo "Time period (years) enter cheyyandi:"
+read time
+
+simple_interest=$((principal * rate * time / 100))
+
+echo "Simple Interest = $simple_interest"
