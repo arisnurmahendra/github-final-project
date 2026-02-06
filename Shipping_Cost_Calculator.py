3645d4bd504474825d945fa2deb8049c1d642999
srinivaaass <gsrinivas69379@gmail.com>
2026-02-06 08:15:02 -0500

    added another line from Srinivaaass

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -2,6 +2,8 @@
 
  # Here is a new update by Srinivaaass
 
+ # Here is another update by Srinivaaass
+
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
