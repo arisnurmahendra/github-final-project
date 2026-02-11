11cc46c5648bf6d7b81a12cbd53d93dd4fe33ef2
Schna Germain <schnagermain@gmail.com>
2026-02-11 18:25:13 -0500

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit 64dca9adc337afe34557d4fdceda45da92c3a9d1.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,5 +11,4 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
  # Aquí hay una nueva actualización por <Schna Germain>
- # Here is another update by <your GitHub username>
 
