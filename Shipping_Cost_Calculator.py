7608defdef87683307d60586bee5938b023c0cc5
aravsi77 <yudiisvara@gmail.com>
2026-02-15 12:36:20 +0100

    add euro feture

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -6,7 +6,9 @@ rate = float(input("Enter the shipping rate per kilogram: "))
 
 ## Calculate shipping cost
 shipping_cost = weight * rate
+shipping_cost_eur = shipping_cost * 0.84
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
+print(f"Shipping Cost: {shipping_cost_eur} EUR")
 
