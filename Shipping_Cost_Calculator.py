0f8f85982930be8d74080ee9fc8519ea726dc24b
anantjadhav1 <anantjadhav2675@gmail.com>
2026-03-01 08:06:43 -0500

    Revert "another change by user"
    This reverts commit 63c08dc4fe27eff6b59cacb3f200938ae587ef1d.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,6 +1,5 @@
 # Shipping Cost Calculator
  # Here is a new update by anant jadhav
- #here is the another change by anant 
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
