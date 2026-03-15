927dbff60b1e1f4783c0a923b7f9b291efa38afe
Jose <jg32389@gmail.com>
2026-03-15 19:44:27 -0300

    Revert "added a new line to Shipping_..."
    This reverts commit f9a5a965ab1b35fc3b2854fd6e2bfa317dafc77a.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -6,7 +6,6 @@ rate = float(input("Enter the shipping rate per kilogram: "))
 
 ## Calculate shipping cost
 shipping_cost = weight * rate
-# Here is a new update by Teknuz
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
