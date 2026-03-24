96671a575cacd67095e2f5480990f41cb704c9be
JuanoxX <bolivarjuanv@gmail.com>
2026-03-24 18:33:43 -0400

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,3 +11,4 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
  # Aquí hay una nueva actualización por <your GitHub username>
+# Here is another update by <your GitHub username>
