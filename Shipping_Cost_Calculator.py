8898866ed9ea1272c01ed2f8be023fae131b6374
jtpnueng <nueng_jatu_2543@hotmail.com>
2026-04-08 16:13:01 -0400

    gg

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,6 +10,31 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
-that ship that is fucking hard!!!
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.multiclass import OneVsOneClassifier
+from sklearn.metrics import accuracy_score
 
-sdfjlsdfsdf
+import warnings
+warnings.filterwarnings('ignore')
+
+file_path = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/GkDzb7bWrtvGXdPOfk6CIg/Obesity-level-prediction-dataset.csv"
+data = pd.read_csv(file_path)
+data.head()
+
+# Standardizing continuous numerical features
+continuous_columns = data.select_dtypes(include=['float64']).columns.tolist()
+
+scaler = StandardScaler()
+scaled_features = scaler.fit_transform(data[continuous_columns])
+
+# Converting to a DataFrame
+scaled_df = pd.DataFrame(scaled_features, columns=scaler.get_feature_names_out(continuous_columns))
+
+# Combining with the original dataset
+scaled_data = pd.concat([data.drop(columns=continuous_columns), scaled_df], axis=1)
