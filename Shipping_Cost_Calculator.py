7f020b29240a56562bd77dd5aa0d592b61e65208
t6102678-maker <t6102678@gmail.com>
2026-04-17 10:39:28 -0400

    Revert "Adding another update for Shipping_Cost_Calculator.py"
    This reverts commit 4f23fea119e0919ec387818e78ea0de13aa814a0.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,6 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
-# Here is a new update by t6102678-maker
-
-# Here is another update by 6102678-maker
+Here is a new update by t6102678-maker
