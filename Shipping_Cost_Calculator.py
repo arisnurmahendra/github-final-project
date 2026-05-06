55b3722c3fa17d830481fce470ae23c53a273f5e
Johnathan Yew <johnathan.yew@gmail.com>
2026-05-06 01:45:35 -0400

    added another line to shipping_calculator

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,4 +1,5 @@
 # Shipping Cost Calculator <Johnathan Yew>
+# Another update by Johnathan Yew
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
