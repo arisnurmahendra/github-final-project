eca0e828ccbf3f59f09dd8af75f540872d82e829
gigazej <josedasilveirajw@gmail.com>
2026-05-08 07:14:00 -0400

    Revert "adjusted_some_problem"
    This reverts commit 483f11822dacb9e0b8674c3a97d459f17b603174.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,5 +1,5 @@
 # Shipping Cost C # Here is a new update by 
-#let me update this here real quick#
+
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
