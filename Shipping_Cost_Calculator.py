a929c00ba53ed935dec6afbf3f2c2a92a7489f43
RPG2025 <rperomingo@sinregistrar.es>
2026-05-10 13:50:38 +0200

    added a new line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,5 +1,5 @@
 # Shipping Cost Calculator
-# Here is a new update by <RPG2025>
+# Here is a new update by RPG2025
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
