930db765343b199e5d3d786f20abcb2862a343e9
Subhadeep Bakshi <subhadeep.bakshi@sail.in>
2026-05-14 03:50:20 -0400

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,5 +1,6 @@
 # Shipping Cost Calculator
-#  # Here is a new update by Subhadeep Bakshi
+# Here is a new update by Subhadeep Bakshi
+# Here is another update by Subhadeep Bakshi
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
