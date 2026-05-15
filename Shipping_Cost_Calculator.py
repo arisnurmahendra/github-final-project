3ac3635b7dac755dee1fc360a158a95a783d2df7
Rexford Holland <hollandrexford.edu@gmail.com>
2026-05-15 15:56:27 -0400

    Revert "updated shipping_cost.py"
    This reverts commit a132498d40afc51a5bc78abf48e1ff475f0b33f6.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,6 +10,6 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- ## Here is another update by  <rexholland-p>
+ # Here is a new update by <rexholland-p>
 
 
