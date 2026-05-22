e639d228eeabda92526c082743ca71b6bdd6b5b5
Gordon <gordon.smith@loreal.com>
2026-05-22 11:19:26 -0400

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit a01a4aa8eb99f5eb72df59d7a4da315b70896f0b.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -2,8 +2,6 @@
 
  # Here is a new update by WestieTowers
 
-# Here is another update by WestieTowers
-
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
