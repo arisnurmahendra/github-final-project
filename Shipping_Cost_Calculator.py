848b961e193056f90a809d891f32743e1f2d633d
rollinhand <rollin.hand@gmx.de>
2026-05-22 10:40:55 +0200

    Revert "added a new line to Shipping_Cost_Calculator.py"
    This reverts commit 2d478e60dbdc2d77498b70054cd33ea299001120.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,5 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
-# Here is a new update by rollinhand
-# Here is another update by rollinhand
+ # Here is a new update by rollinhand
