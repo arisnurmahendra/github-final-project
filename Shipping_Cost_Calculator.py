7543a3829aecf6dcc9d74742ef333081051729fa
Ajhathashatru <pawank8k@gmail.com>
2026-05-24 09:41:48 -0400

    another commit

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,4 +10,5 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by Ajhathashatru
+ # Here is a new update by Ajhathashatru+
+ #here is an another update 
