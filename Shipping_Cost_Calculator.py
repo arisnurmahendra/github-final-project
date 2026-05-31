909927fda6d4e2782693e1aed0fc1538af9a6365
radoslav-1973 <radoslav-1973@outlook.com>
2026-05-31 13:30:31 +0300

    Twice as expensive outside Sofia

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -3,9 +3,10 @@
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
+location = int(input("Enter 1 for Sofia and 2 for province:"))
 
 ## Calculate shipping cost
-shipping_cost = weight * rate
+shipping_cost = weight * rate * location
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
