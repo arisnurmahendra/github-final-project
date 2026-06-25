c62931a46a10765a7c33f55e1d1314d111d90e2a
Erick92-unitec <erick_esau@unitec.edu>
2026-06-25 13:21:53 -0400

    added a new line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,5 +10,5 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by Erick92-unitec
+### Here is a new update by Erick92-unitec
 
