b4d125cdb620b0aa5a2e5fa3648a9e152914e7e8
Participant <your-email@example.com>
2026-06-30 14:39:19 -0400

    Another line

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,4 +10,5 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by the course partic
+ # Here is a new update by the course participant
+# Here is another update by <your GitHub username>
