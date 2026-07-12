3f115cfe08bc6ef544b98c2d874ddae6ea41a725
Ahmed Ghaffar <ahmed.ghaffar555@gmail.com>
2026-07-12 23:55:30 +0500

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit 18f80199d70b8029d11978b01f5636ff3dce0beb.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,8 +1,7 @@
 # Shipping Cost Calculator
 
- # Here is a new update by Ahmed Ghaffar
- # Here is another update by Ahmed Ghaffar
-
+ # Here is a new update by <Ahmed Ghaffar>
+ 
 
 
 ## Input package weight and shipping rate
