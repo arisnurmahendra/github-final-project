31c038a135a87f7b0d44222ca90772255b13b6f0
Ricardo Shimoda Nakasako <rshimoda@gmail.com>
2026-07-13 15:09:00 -0400

    Revert "more changes"
    This reverts commit 78a6e5c0edd014a8a55d8d84ef47a57ee905c775.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,6 +1,5 @@
 # Shipping Cost Calculator
 # Here is a new update by ricardoshimoda
-#  Here is another update by ricardoshimoda
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
