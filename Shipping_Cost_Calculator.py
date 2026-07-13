1aad94d0dc2c57609188fc0833dea8409b7b8313
Tomas <tomaschamorro97@hotmail.com>
2026-07-13 01:39:49 -0400

    Revert "Added new line of code to shipping_cost_calculator"
    This reverts commit 7d12c6cea80f431838f78408adb968f6c946cc9b.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,7 +1,6 @@
 # Shipping Cost Calculator
 
-# Here is a new update by tomaschamorro97
-# Here is another update by <your GitHub username>
+ # Here is a new update by tomaschamorro97
 
 
 ## Input package weight and shipping rate
