23c672bdb1c5ffe112649e5f388e20adb27bd069
Naing Lu Tun <nainglu7924@gmail.com>
2026-07-17 10:15:02 +0630

    Revert "added another update to Shipping_Cost_Calculator.py"
    This reverts commit 1f1245fd2171f746e61261fb3d1345241c00e25e.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,6 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by Naing Lu Tun
-
- # Here is another update by Naing Lu Tun
+ # Here is a new update by <your GitHub username>
